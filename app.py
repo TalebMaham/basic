7787fdd745eea42e684501ddcca122be23cf2441
TalebMaham <sidnas9@gmail.com>
2025-02-19 18:08:23 +0100

    add two tests

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
+@app.route('/test1')
+def test1():
+    return "Test 1 Passed"
+
+@app.route('/test2')
+def test2():
+    return "Test 2 Passed"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- test_app.py
+++ test_app.py
@@ -3,11 +3,27 @@ from app import app
 
 class FlaskTestCase(unittest.TestCase):
 
+    def setUp(self):
+        """Initialisation du client de test"""
+        self.app = app.test_client()
+
     def test_hello(self):
-        tester = app.test_client(self)
-        response = tester.get('/')
+        """Test de la route '/'"""
+        response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b"Hello, World!")
+
+    def test_test1(self):
+        """Test de la route '/test1'"""
+        response = self.app.get('/test1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b"Test 1 Passed")
+
+    def test_test2(self):
+        """Test de la route '/test2'"""
+        response = self.app.get('/test2')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b"Test 2 Passed")
 
 if __name__ == '__main__':
     unittest.main()
